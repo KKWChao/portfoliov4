014d0c2649e67ea21913d3965f0395f59ceca64f
Kelvin Chao <kelvinchao2011@gmail.com>
2023-07-02 16:43:12 -0700

    added tech

--- src/scenes/Projects/index.jsx
+++ src/scenes/Projects/index.jsx
@@ -19,6 +19,7 @@ const projects = [
       "Cyberwares is a sleek e-commerce platform offering cutting-edge tech products. Powered by Next.js, it delivers a seamless shopping experience with responsive design. Secure payments are facilitated by Stripe integration. The admin site, built with Next.js, NextAuth, Tailwind CSS, and Chart.js, enables efficient product and category management. Explore and purchase the latest tech innovations with Cyberwares.",
     projectLink: "https://github.com/KKWChao/CyberwaresFrontend",
     githubLink: "https://cyberwares-frontend.vercel.app/",
+    technologies: ["NextJS", "MongoDB", "Node.js", "Stripe"],
   },
   {
     id: 1,
@@ -29,6 +30,7 @@ const projects = [
     projectLink: "https://github.com/KKWChao/YoutubeClone",
     githubLink:
       "https://632a22c204beb617c6d1078a--meek-marzipan-6344b2.netlify.app/",
+    technologies: ["React", "MUI", "API"],
   },
 
   {
@@ -39,16 +41,18 @@ const projects = [
       "Discover EvoGym Demo, a sleek and interactive fitness website that showcases the features and capabilities of EvoGym. Explore various workout routines, browse fitness classes, and learn about personalized training programs. With its modern design and user-friendly interface, EvoGym Demo offers an immersive experience for fitness enthusiasts looking to achieve their health and wellness goals.",
     projectLink: "https://evogym-demo.pages.dev/",
     githubLink: "https://github.com/KKWChao/DemoSite",
+    technologies: ["React", "TypeScript", "TailwindCSS"],
   },
-  // {
-  //   id: 3,
-  //   imageSrc: "./images/Project4.png",
-  //   projectName: "Sociopedia",
-  //   projectDescription:
-  //     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis corporis ab itaque tempora accusamus quas excepturi? Facere ex alias autem corrupti exercitationem, repellat fuga placeat qui labore, eos laboriosam magnam?",
-  //   projectLink: "",
-  //   githubLink: "#",
-  // },
+  {
+    id: 3,
+    imageSrc: "./images/Project4.png",
+    projectName: "Sociopedia",
+    projectDescription:
+      "A social media site to show the world your personality. ",
+    projectLink: "",
+    githubLink: "https://github.com/KKWChao/FullstackSite",
+    technologies: ["MongoDB", "React", "Node.js", "MUI"],
+  },
 ];
 
 function Projects() {
@@ -58,19 +62,29 @@ function Projects() {
     projectLink,
     projectDescription,
     githubLink,
+    technologies,
   }) => {
     return (
-      <div className="overflow-hidden rounded-lg text-white bg-transparent border-4 border-white md:m-20 m-2 p-4 flex flex-col justify-center items-center bg-black">
+      <div className="overflow-hidden rounded-lg text-white border-4 border-white md:m-20 m-2 px-2 flex flex-col justify-center items-center bg-black">
         <h1 className="py-5 md:text-3xl">{projectName}</h1>
-        <img src={imageSrc} alt={projectName} className="" />
-        <div className="flex flex-col justify-center items-center">
-          <div className=" p-4">
+        <div className="w-full md:w-3/4 ">
+          <img src={imageSrc} alt={projectName} className="rounded-md" />
+        </div>
+
+        <div className="flex flex-col justify-center items-center w-full md:w-3/4 ">
+          <div className="">
             <p className="py-2 lg:text-xl lg:w-1/2 text-sm">
               {projectDescription}
             </p>
+            <h1>Technologies</h1>
+            <div className="flex justify-center pt-1 text-sm gap-3">
+              {technologies.map((tech) => (
+                <p key={`${tech}`}>{tech}</p>
+              ))}
+            </div>
           </div>
 
-          <div className="flex justify-center gap-10 md:mt-5 align-middle">
+          <div className="flex justify-around w-full md:my-5 align-middle">
             <a
               href={projectLink}
               className="hover:bg-gray-500 px-2 py-1 rounded-md"
@@ -79,6 +93,7 @@ function Projects() {
             >
               <LaunchIcon fontSize="large" />
             </a>
+
             <a
               href={githubLink}
               className="hover:bg-gray-500 px-2 py-1 rounded-md"
@@ -99,6 +114,7 @@ function Projects() {
     projectLink: PropTypes.string,
     projectDescription: PropTypes.string,
     githubLink: PropTypes.string,
+    technologies: PropTypes.array,
   };
 
   return (
@@ -106,7 +122,7 @@ function Projects() {
       id="projects"
       className="absolute text-white top-[250vh] w-full text-center overflow-hidden"
     >
-      <h1 className="text-5xl pt-10 title">Projects</h1>
+      <h1 className="text-5xl pt-5 title">Projects</h1>
 
       <Swiper
         grabCursor={true}
@@ -127,7 +143,7 @@ function Projects() {
         resizeObserver={true}
         loop={true}
         modules={[EffectCoverflow, Autoplay, Pagination]}
-        className="md:mt-0 md:w-1/2"
+        className="md:mt-0 md:w-1/2 mt-[20%]"
       >
         {projects.map((item) => (
           <SwiperSlide key={`${item.projectName}_${item.id}`} className="">
@@ -137,11 +153,12 @@ function Projects() {
               projectDescription={item.projectDescription}
               projectLink={item.projectLink}
               githubLink={item.githubLink}
+              technologies={item.technologies}
             />
           </SwiperSlide>
         ))}
       </Swiper>
-      <p className="text-sm mt-5">Swipe or drag the items...</p>
+      <p className="text-sm mt-3">Swipe or drag the items...</p>
     </section>
   );
 }
